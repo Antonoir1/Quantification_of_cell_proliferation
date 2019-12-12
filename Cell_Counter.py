3159fae438000f6d631063b216cdeb910d416a0d
Valentin <toninvalet@gmail.com>
2019-12-12 09:40:51 +0100

    Adding callout

--- Cell_Counter.py
+++ Cell_Counter.py
@@ -1,11 +1,8 @@
-import sys
-import os
+import sys, os, math
+import numpy as np
 from PySide2.QtWidgets import QApplication, QMainWindow
 from PySide2 import QtCore, QtGui, QtWidgets
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.figure import Figure
-import matplotlib.image as mpimg
-import numpy as np
+from PySide2.QtCharts import QtCharts
 
 import time, random
 
@@ -53,36 +50,236 @@ class InputData(QtWidgets.QWidget):
     def get_path(self):
         return str(self.label3.text())
 
+#CALL TO HANDLE THE CALLOUTS ON THE GRAPH
+class Callout(QtWidgets.QGraphicsItem):
+    def __init__(self, chart):
+        QtWidgets.QGraphicsItem.__init__(self, chart)
+        self._chart = chart
+        self._text = ""
+        self._textRect = QtCore.QRectF()
+        self._anchor = QtCore.QPointF()
+        self._font = QtGui.QFont()
+        self._rect = QtCore.QRectF()
+
+    def boundingRect(self):
+        anchor = self.mapFromParent(self._chart.mapToPosition(self._anchor))
+        rect = QtCore.QRectF()
+        rect.setLeft(min(self._rect.left(), anchor.x()))
+        rect.setRight(max(self._rect.right(), anchor.x()))
+        rect.setTop(min(self._rect.top(), anchor.y()))
+        rect.setBottom(max(self._rect.bottom(), anchor.y()))
+
+        return rect
+
+    def paint(self, painter, option, widget):
+        path = QtGui.QPainterPath()
+        path.addRoundedRect(self._rect, 5, 5)
+        anchor = self.mapFromParent(self._chart.mapToPosition(self._anchor))
+        if not self._rect.contains(anchor) and not self._anchor.isNull():
+            point1 = QtCore.QPointF()
+            point2 = QtCore.QPointF()
+
+            # establish the position of the anchor point in relation to _rect
+            above = anchor.y() <= self._rect.top()
+            aboveCenter = (anchor.y() > self._rect.top() and
+                anchor.y() <= self._rect.center().y())
+            belowCenter = (anchor.y() > self._rect.center().y() and
+                anchor.y() <= self._rect.bottom())
+            below = anchor.y() > self._rect.bottom()
+
+            onLeft = anchor.x() <= self._rect.left()
+            leftOfCenter = (anchor.x() > self._rect.left() and
+                anchor.x() <= self._rect.center().x())
+            rightOfCenter = (anchor.x() > self._rect.center().x() and
+                anchor.x() <= self._rect.right())
+            onRight = anchor.x() > self._rect.right()
+
+            # get the nearest _rect corner.
+            x = (onRight + rightOfCenter) * self._rect.width()
+            y = (below + belowCenter) * self._rect.height()
+            cornerCase = ((above and onLeft) or (above and onRight) or
+                (below and onLeft) or (below and onRight))
+            vertical = abs(anchor.x() - x) > abs(anchor.y() - y)
+
+            x1 = (x + leftOfCenter * 10 - rightOfCenter * 20 + cornerCase *
+                int(not vertical) * (onLeft * 10 - onRight * 20))
+            y1 = (y + aboveCenter * 10 - belowCenter * 20 + cornerCase *
+                vertical * (above * 10 - below * 20))
+            point1.setX(x1)
+            point1.setY(y1)
+
+            x2 = (x + leftOfCenter * 20 - rightOfCenter * 10 + cornerCase *
+                int(not vertical) * (onLeft * 20 - onRight * 10))
+            y2 = (y + aboveCenter * 20 - belowCenter * 10 + cornerCase *
+                vertical * (above * 20 - below * 10))
+            point2.setX(x2)
+            point2.setY(y2)
+
+            path.moveTo(point1)
+            path.lineTo(anchor)
+            path.lineTo(point2)
+            path = path.simplified()
+
+        painter.setBrush(QtGui.QColor(255, 255, 255))
+        painter.drawPath(path)
+        painter.drawText(self._textRect, self._text)
+
+    def mousePressEvent(self, event):
+        event.setAccepted(True)
+
+    def mouseMoveEvent(self, event):
+        if event.buttons() & QtCore.Qt.LeftButton:
+            self.setPos(self.mapToParent(
+                event.pos() - event.buttonDownPos(QtCore.Qt.LeftButton)))
+            event.setAccepted(True)
+        else:
+            event.setAccepted(False)
+
+    def setText(self, text):
+        self._text = text
+        metrics = QtGui.QFontMetrics(self._font)
+        self._textRect = QtCore.QRectF(metrics.boundingRect(
+            QtCore.QRect(0.0, 0.0, 150.0, 150.0),QtCore.Qt.AlignLeft, self._text))
+        self._textRect.translate(5, 5)
+        self.prepareGeometryChange()
+        self._rect = self._textRect.adjusted(-5, -5, 5, 5)
+
+    def setAnchor(self, pointX, pointY):
+        self._anchor = QtCore.QPointF(pointX, pointY)
+
+    def updateGeometry(self):
+        self.prepareGeometryChange()
+        self.setPos(self._chart.mapToPosition(
+            self._anchor) + QtCore.QPointF(10, -50))
+
 
 #CLASS TO HANDLE THE POPULATION GRAPH
-class PopulationCanvas(FigureCanvas):
-    def __init__(self, parent=None, width=5, height=4, dpi=100):
-        fig = Figure(figsize=(width, height), dpi=dpi)
-        self.axes = fig.add_subplot(111)
-        FigureCanvas.__init__(self, fig)
-        self.setParent(parent)
-        FigureCanvas.setSizePolicy(self,QtWidgets.QSizePolicy.Expanding,QtWidgets.QSizePolicy.Expanding)
-        FigureCanvas.updateGeometry(self)
+class PopulationGraph(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+        layout = QtWidgets.QHBoxLayout()
+        layout.setSpacing(0)
+        layout.setContentsMargins(0,0,0,0)
 
-    def update_figure(self,X,Y):
-        self.axes.cla()
-        self.axes.plot(X, Y, 'r')
-        self.draw()
+        self.View = QtCharts.QChartView()
+        self.View.setRubberBand(QtCharts.QChartView.HorizontalRubberBand)
+        self.View.setRenderHint(QtGui.QPainter.RenderHint.Antialiasing)
+        self.line = QtCharts.QLineSeries()
+        self.area = QtCharts.QAreaSeries(self.line)
+        self.area.setBrush(QtGui.QBrush(QtCore.Qt.red))
+        self.area.setBorderColor(QtCore.Qt.gray)
+        self.area.setName("Cells")
+        self.chart = QtCharts.QChart()
+        self.chart.setTheme(self.chart.ChartThemeBlueIcy)
+        self.chart.addSeries(self.area)
+        self.chart.createDefaultAxes()
+        self.View.setChart(self.chart)
 
-class PopulationImage(FigureCanvas):
-    def __init__(self, parent=None, width=5, height=4, dpi=100):
-        fig = Figure(figsize=(width, height), dpi=dpi)
-        self.axes = fig.add_subplot(111)
-        FigureCanvas.__init__(self, fig)
-        self.setParent(parent)
-        FigureCanvas.setSizePolicy(self,QtWidgets.QSizePolicy.Expanding,QtWidgets.QSizePolicy.Expanding)
-        FigureCanvas.updateGeometry(self)
+        layout.addWidget(self.View)
+        self.setLayout(layout)
+        self._tooltip = Callout(self.chart)
+        self.area.hovered.connect(self.tooltip)
+        self.setMouseTracking(True)
+        self.X = []
+        self.Y = []
 
-    def update_figure(self,path):
-        img = mpimg.imread(path)
-        self.axes.cla()
-        self.axes.imshow(img)
-        self.draw()
+    #Handle the Callouts on the graph
+    def tooltip(self, point, state):
+        if self._tooltip == 0:
+            self._tooltip = Callout(self._chart)
+        if state:
+            for i in range(0,len(self.X)):
+                if(i == 0):
+                    if point.x() < (self.X[i]+0.5):
+                        self._tooltip.setText("X: {0:.2f} \nY: {1:.2f} ".format(self.X[i],self.Y[i]))
+                        self._tooltip.setAnchor(self.X[i],self.Y[i])
+                        self._tooltip.setZValue(11)
+                        self._tooltip.updateGeometry()
+                        self._tooltip.show()
+                        break
+                elif(i == len(self.X)-1):
+                    if point.x() >= self.X[i] - 0.5:
+                        self._tooltip.setText("X: {0:.2f} \nY: {1:.2f} ".format(self.X[i],self.Y[i]))
+                        self._tooltip.setAnchor(self.X[i],self.Y[i])
+                        self._tooltip.setZValue(11)
+                        self._tooltip.updateGeometry()
+                        self._tooltip.show()
+                        break
+                else:
+                    if point.x() >= self.X[i] - 0.5 and point.x() < (self.X[i]+0.5):
+                        self._tooltip.setText("X: {0:.2f} \nY: {1:.2f} ".format(self.X[i],self.Y[i]))
+                        self._tooltip.setAnchor(self.X[i],self.Y[i])
+                        self._tooltip.setZValue(11)
+                        self._tooltip.updateGeometry()
+                        self._tooltip.show()
+                        break
+        else:
+            self._tooltip.hide()
+    
+    #Modify the graph
+    def setPopulation(self,X,Y):
+        self.X = X
+        self.Y = Y
+        self.line.clear()
+        for i in range(0,len(X)):
+            self.line.append(X[i],Y[i])
+        self.chart.removeSeries(self.area)
+        self.chart.addSeries(self.area)
+        self.chart.createDefaultAxes()
+
+#CLASS TO HANDLE THE IMAGES OF THE POPULATION
+class PopulationImages(QtWidgets.QGraphicsView):
+    def __init__(self, parent=None):
+        super(PopulationImages, self).__init__(parent)
+        self.setRenderHints(QtGui.QPainter.Antialiasing |
+                QtGui.QPainter.HighQualityAntialiasing)
+        self.setViewportUpdateMode(QtWidgets.QGraphicsView.FullViewportUpdate)
+        self.setResizeAnchor(QtWidgets.QGraphicsView.AnchorViewCenter)
+        self.setTransformationAnchor(QtWidgets.QGraphicsView.AnchorUnderMouse)
+        self.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAsNeeded)
+        self.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAsNeeded)
+        self.home = self.matrix()
+        self.viewstyle = 'background-color: white;'
+        self.setStyleSheet(self.viewstyle)
+
+        self._scene = QtWidgets.QGraphicsScene(self)
+        self._photo = QtWidgets.QGraphicsPixmapItem()
+        self._scene.addItem(self._photo)
+        self.setScene(self._scene)
+
+    #Change the image displayed
+    def Display(self, img):
+        pixmap = QtGui.QPixmap(img)
+        self.setDragMode(QtWidgets.QGraphicsView.ScrollHandDrag)
+        self._photo.setPixmap(pixmap)
+        self.fitInView(self._photo,QtCore.Qt.KeepAspectRatio)
+
+    def wheelEvent(self, event):
+        if event.type() == QtCore.QEvent.Wheel:
+            self.scaleView(math.pow(2.0, -event.delta() / 500.0))
+            super(PopulationImages, self).wheelEvent(event)
+        else:
+            event.ignore()
+
+    def scaleView(self, factor):
+        f = self.matrix().scale(factor, factor). \
+                    mapRect(QtCore.QRectF(0, 0, 1, 1)).width()
+        if f < 0.05 or f > 50:
+            return
+        self.scale(factor, factor)
+
+#Main window Scroll
+class Scroll(QtWidgets.QScrollArea):
+    def __init__(self, parent=None):
+        super(Scroll, self).__init__(parent)
+        self.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
+        self.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.setWidgetResizable(True)
+        self.setMinimumSize(600,600)
+    
+    def wheelEvent(self, event):
+        if event.type() == QtCore.QEvent.Wheel:
+            event.ignore()
 
 
 #CLASS TO HANDLE THE WINDOW
@@ -163,7 +360,7 @@ class Window(QtWidgets.QWidget):
         labelgraph.setMinimumWidth(175)
         labelgraph.setMaximumWidth(200)
         labelgraph.setContentsMargins(0,50,0,15)
-        self.Graph = PopulationCanvas(self, width=5, height=4, dpi=115)
+        self.Graph = PopulationGraph()
         self.Graph.setMinimumHeight(300)
 
         #GLOBAL VARIABLES FOR PROCESSING IMAGES
@@ -179,9 +376,8 @@ class Window(QtWidgets.QWidget):
         layoutimg = QtWidgets.QHBoxLayout()
         layoutimg.setContentsMargins(0,0,0,0)
         layoutimg.setSpacing(0)
-        self.output_imgs = PopulationImage(self, width=5, height=4, dpi=115)
-        self.output_imgs.setMinimumHeight(300)
-        self.output_imgs.setMinimumWidth(300)
+        self.View = PopulationImages(self)
+        self.View.setMinimumHeight(300)
         self.labelimg = QtWidgets.QLabel("Population image:")
         self.labelimg.setObjectName("population_img")
         self.labelimg.setAlignment(QtCore.Qt.AlignBottom)
@@ -200,7 +396,7 @@ class Window(QtWidgets.QWidget):
         backbutton.setSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Expanding)
         backbutton.clicked.connect(self.Back)
         layoutimg.addWidget(backbutton)
-        layoutimg.addWidget(self.output_imgs)
+        layoutimg.addWidget(self.View)
         layoutimg.addWidget(nextbutton)
 
         #SET THE LAYOUTS (GLOBAL LAYOUT, INPUT LAYOUT, OUTPUT LAYOUT)
@@ -222,17 +418,14 @@ class Window(QtWidgets.QWidget):
         Container = QtWidgets.QWidget()
         Container.setLayout(self.layout)
         Container.setObjectName("body")
-        Scroll = QtWidgets.QScrollArea()
-        Scroll.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
-        Scroll.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
-        Scroll.setWidgetResizable(True)
-        Scroll.setMinimumSize(600,600)
-        Scroll.setWidget(Container)
+
+        self.scroll = Scroll()
+        self.scroll.setWidget(Container)
 
         #SCROLLING BAR LAYOUT
         vlayout = QtWidgets.QVBoxLayout(self)
         vlayout.setContentsMargins(0,0,0,0)
-        vlayout.addWidget(Scroll)
+        vlayout.addWidget(self.scroll)
         self.setLayout(vlayout)
 
     #GO TO THE NEXT IMAGE
@@ -240,15 +433,32 @@ class Window(QtWidgets.QWidget):
         if(len(self.result)):
             if(self.position < (len(self.result)-1)):
                 self.position += 1
-                self.output_imgs.update_figure(self.path+"/"+self.result[self.position])
-                self.labelimg.setText("Population image: "+str(self.position)+"/"+str(len(self.result)))
+                self.View.Display(self.path+"/"+self.result[self.position])
+                self.labelimg.setText("Population image: "+str(self.position+1)+"/"+str(len(self.result))+" Cells: "+str(self.Y[self.position]))
 
     #GO BACK TO THE LAST IMAGE
     def Back(self):
         if(self.position > 0):
                 self.position -= 1
-                self.output_imgs.update_figure(self.path+"/"+self.result[self.position])
-                self.labelimg.setText("Population image: "+str(self.position)+"/"+str(len(self.result)))
+                self.View.Display(self.path+"/"+self.result[self.position])
+                self.labelimg.setText("Population image: "+str(self.position+1)+"/"+str(len(self.result))+" Cells: "+str(self.Y[self.position]))
+
+    #SHORTCUTS: NEXT IMAGE (CTRL + N)  LAST IMAGE(CTRL + B) 
+    def keyPressEvent(self, event):
+        if(event.key() == QtCore.Qt.Key_Return):
+            self.Process()
+        elif(event.key() == QtCore.Qt.Key_Escape):
+            QApplication.quit()
+        else:
+            k = event.key()
+            m = int(event.modifiers())
+            
+            if QtGui.QKeySequence(m+k) == QtGui.QKeySequence('Ctrl+N'):
+                self.Next()
+            elif QtGui.QKeySequence(m+k) == QtGui.QKeySequence('Ctrl+B'):
+                self.Back()
+            else:
+                event.ignore()
 
     #CHECK THE PATHS OF THE INPUT/PREFIX FIELD AND LAUNCH THE PROCESS OF COUNTING
     def Process(self):
@@ -315,13 +525,13 @@ class Window(QtWidgets.QWidget):
                         self.Y.append(i+random.randint(0,25))
                         work += (1/(len(imgs)-1))*100
                         self.progress.setValue(work)
-                        
-                    self.Graph.update_figure(self.X,self.Y)
-                    self.output_imgs.update_figure(self.ind.get_path()+"/"+self.result[0])
+                    
+                    self.Graph.setPopulation(self.X,self.Y)
+                    self.View.Display(self.ind.get_path()+"/"+self.result[0])
                     self.position = 0
                     self.path = self.ind.get_path()
                     self.prefix = str(self.inputpref.text())
-                    self.labelimg.setText("Population image: 0/"+str(len(self.result)))
+                    self.labelimg.setText("Population image: 1/"+str(len(self.result))+" Cells: "+str(self.Y[0]))
                         
 
 
